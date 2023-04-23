1ff69c3281749f853276844fa5d42fa14a21c031
Alyxia <alyxia@riseup.net>
2023-04-23 22:34:29 +0200

    [Main] Some funny Abacus UI

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,7 +1,8 @@
-import org.gtkkn.bindings.gio.ApplicationFlags
+import org.gtkkn.bindings.gio.*
+import org.gtkkn.bindings.gtk.*
+import org.gtkkn.bindings.he.*
 import org.gtkkn.bindings.he.Application
 import org.gtkkn.bindings.he.ApplicationWindow
-import org.gtkkn.bindings.he.FillButton
 import org.gtkkn.extensions.gtk.setMargins
 
 private const val APP_ID = "org.gtkkn.samples.gtk.playground"
@@ -10,18 +11,58 @@ fun main() {
     // Create a new application
     val app = Application(APP_ID, ApplicationFlags.FLAGS_NONE)
 
+    val mainMenu = Menu().apply {
+        insertItem(0, MenuItem("About Abacus", "win.action-about"))
+    }
+
     // Connect to "activate" signal of `app`
     app.connectActivate {
+        val box = Box(Orientation.VERTICAL, 5)
+
+        val appBar = AppBar()
+        appBar.setShowBack(false)
+        appBar.setViewtitleWidget(DropDown(listOf("Temperature", "Mass", "Length")).apply {
+            visible = false
+            halign = Align.START
+            cssClasses = listOf("view-switcher")
+        })
+
+        appBar.append(MenuButton().apply {
+            menuModel = mainMenu
+            setIconName("open-menu")
+        })
+
+        box.append(appBar)
+
         val button = FillButton("what")
         button.setMargins(12)
         button.connectClicked {
             button.setLabel("WHAT")
         }
+        box.append(button)
 
         // Create a window and set the title
         val window = ApplicationWindow(app)
         window.setTitle("My GTK App")
-        window.setChild(button)
+        window.setChild(box)
+
+        val aboutAction = SimpleAction("action-about", null)
+        aboutAction.connectActivate {
+            val aboutWindow = AboutWindow().apply {
+                title = "Abacus"
+                setAppId("com.fyralabs.Abacus")
+                setVersion("0.0.0")
+                setMoreInfoUrl("https://fyralabs.com")
+                setTranslateUrl("https://fyralabs.com")
+                setIssueUrl("https://fyralabs.com")
+                setDeveloperNames(listOf("Fyra Labs"), 1)
+                setCopyrightYear(2022)
+                setLicense(AboutWindowLicenses.GPLV3)
+                setColor(Colors.PURPLE)
+            }
+            aboutWindow.present()
+        }
+        window.addAction(aboutAction)
 
         // Present window
         window.present()
